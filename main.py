738139da0f0d301fedd7183ac672bbca6ed69103
Shucheng <chaoshuchenc@gmail.com>
2021-04-27 06:42:46 -0700

    changed approx corner order; added commentating tools

--- main.py
+++ main.py
@@ -3,51 +3,105 @@ import numpy as np
 import utils
 
 stream = True
-path = 'sampleTable.jpg'
-cap = cv2.VideoCapture('red_ball_1.mp4')
-cap.set(3, 608)  # width
-cap.set(4, 1080)  # height
+path = 'youtube_game.png'
+cap = cv2.VideoCapture('drill_fast.mp4')
+cap.set(3, 608)  # set width
+cap.set(4, 1080)  # set height
 wTable = 54  # inches
 hTable = 108  # inches
 scale = 4
+wBirdseye = wTable * scale  # pixel
+hBirdseye = hTable * scale  # pixel
 
 warpMatrix = np.zeros((3, 3))
-curWeight = 0
+curMatrixWeight = 0
+
+# The following two lists will be used to record the points to be drawn on each window
+birdseyeToDraw = []
+originalToDraw = []
+toDrawLists = [originalToDraw, birdseyeToDraw]
+
+birdseyeLineColor = (0, 200, 200)
+originalLineColor = (200, 200, 0)
+
+mouseX = -1
+mouseY = -1
+
+
+def printXY(event, x, y, flags, param):
+    global mouseY, mouseX, toDrawLists
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # on left button down, add current mouse coords to the target list
+        mouseX, mouseY = x, y
+        toDrawLists[param].append(np.array([[x, y]]))
+        # print(param, mouseX, mouseY, originalToDraw, birdseyeToDraw)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        # on right click, delete the latest point in the target list
+        if len(toDrawLists[param]) > 0:
+            toDrawLists[param].pop()
+    elif event == cv2.EVENT_RBUTTONDBLCLK:
+        # on db right click, delete the first point in the target list
+        if len(toDrawLists[param]) > 0:
+            toDrawLists[param].pop(0)
+
+
+cv2.namedWindow('Original')
+cv2.setMouseCallback('Original', printXY, param=0)
+cv2.namedWindow('Warped Table')
+cv2.setMouseCallback('Warped Table', printXY, param=1)
 
 while cap.isOpened():
     if stream:
         success, img = cap.read()
     else:
-        success, img = True, cv2.imread(path)
+        success = True
+        img = cv2.imread(path)
 
     if not success:
         print("Can't receive frame. Exiting...")
         break
 
+    img = cv2.resize(img, (0, 0), None, 0.7, 0.7)
     img, contours = utils.getContours(img, showCanny=True, draw=True)
     if len(contours) != 0:
         contourMax = contours[0]
         approxCorners = contourMax[2]
-        imgWarp, newMatrix = utils.warpImg(img, approxCorners, wTable * scale, hTable * scale,
-                                           prevMatrix=warpMatrix, prevWeight=curWeight)
+        imgWarp, newMatrix = utils.warpImg(img, approxCorners, wBirdseye, hBirdseye,
+                                           prevMatrix=warpMatrix, prevWeight=curMatrixWeight)
         warpMatrix = newMatrix
-        curWeight += 1
+        curMatrixWeight += 1
+        # now go through all the points that needs to be drawn for both windows:
+        if len(birdseyeToDraw) >= 2:
+            for i in range(len(birdseyeToDraw) - 1):
+                cv2.line(imgWarp, utils.np2tuple(birdseyeToDraw[i]), utils.np2tuple(birdseyeToDraw[i+1]),
+                         birdseyeLineColor)
+                utils.drawWarpedLines(birdseyeToDraw[i], birdseyeToDraw[i + 1],
+                                      np.linalg.inv(warpMatrix), img, color=birdseyeLineColor, thickness=3)
+        if len(originalToDraw) >= 2:
+            for i in range(len(originalToDraw) - 1):
+                cv2.line(img, utils.np2tuple(originalToDraw[i]), utils.np2tuple(originalToDraw[i+1]),
+                         originalLineColor, thickness=3)
+                utils.drawWarpedLines(originalToDraw[i], originalToDraw[i + 1], warpMatrix, imgWarp,
+                                      color=originalLineColor)
         cv2.imshow('Warped Table', imgWarp)
 
-    img = cv2.resize(img, (0, 0), None, 0.7, 0.7)
     cv2.imshow('Original', img)
 
     k = cv2.waitKey(25) or 0xff
     if k == ord('q') or k == 27:
         break
-    if k == ord('p') or k == 32:  # pause/play the video
-        while True:
-            key2 = cv2.waitKey(25) or 0xff
-            cv2.imshow('Original', img)
-
-            if key2 == ord('p') or k == 32:
-                break
-        # cv2.waitKey(-1)  # wait until any key is pressed
+    elif k == ord('p') or k == 32:  # pause/play the video
+        # while True:
+        #     key2 = cv2.waitKey(25) or 0xff
+        #     cv2.imshow('Original', img)
+        #
+        #     if key2 == ord('p') or k == 32:
+        #         break
+        #     elif k == ord('a'):
+        #         print(mouseX, mouseY)
+        cv2.waitKey(-1)  # wait until any key is pressed
+    elif k == ord('a'):
+        print(mouseX, mouseY)
 
 cap.release()
 cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -14,22 +14,23 @@ def filterColor(img, lower, upper, debug=False):
 # returns the approximated four corners of a (trapezoid) contour
 def getCornerApprox(contour):
     hull = cv2.convexHull(contour)
-    # peri = cv2.arcLength(hull, True)
     # print("convex hull result: ", len(hull), hull)
+    # peri = cv2.arcLength(hull, True)
     # approx = cv2.approxPolyDP(hull, 0.05 * peri, True)
+    # approx = reorder4Corners(approx)
     # print("convex approx result: ", len(approx), approx)
 
     # the following algo is more robust than cv2.approxPolyDP()
-    newApprox = np.zeros_like(hull)[:4]
+    approx = np.zeros_like(hull)[:4]
     points = hull.reshape((len(hull), 2))
     add = points.sum(1)
-    newApprox[0] = points[np.argmin(add)]
-    newApprox[3] = points[np.argmax(add)]
+    approx[0] = points[np.argmin(add)]
+    approx[2] = points[np.argmax(add)]
     diff = np.diff(points, axis=1)
-    newApprox[1] = points[np.argmin(diff)]
-    newApprox[2] = points[np.argmax(diff)]
+    approx[1] = points[np.argmin(diff)]
+    approx[3] = points[np.argmax(diff)]
 
-    return newApprox
+    return approx
 
 
 '''
@@ -43,8 +44,22 @@ def getContours(img, cThr=(100, 300), showCanny=False, minArea=1000, draw=False)
     imgBlur = cv2.GaussianBlur(imgBlue, (5, 5), 1)
     imgCanny = cv2.Canny(imgBlur, cThr[0], cThr[1])
     kernel = np.ones((10, 10))
-    imgDilation = cv2.dilate(imgCanny, kernel=kernel, iterations=3)
-    imgThresh = cv2.erode(imgDilation, kernel=kernel, iterations=3)
+    imgDilation = cv2.dilate(imgCanny, kernel=kernel, iterations=2)
+    imgThresh = cv2.erode(imgDilation, kernel=kernel, iterations=1)
+
+    # lower_red = np.array([110, 150, 50])
+    # upper_red = np.array([180, 255, 180])
+    # imgRed = filterColor(img, lower_red, upper_red)
+    # imgRed = cv2.cvtColor(imgRed, cv2.COLOR_BGR2GRAY)
+    # retV, imgRed = cv2.threshold(imgRed, 20, 255, cv2.THRESH_BINARY)
+    # cv2.imshow('Red', imgRed)
+    # circles = cv2.HoughCircles(imgRed, cv2.HOUGH_GRADIENT, 1, 100)
+    # print(circles)
+    # circles = circles[0, :, :]
+    # circles = np.uint16(np.around(circles))
+    # for c in circles[:]:
+    #     cv2.circle(img, (c[0], c[1]), c[2], (0, 255, 0), 5)
+    #     cv2.circle(img, (c[0], c[1]), 2, (0, 255, 0), 10)
 
     contours, hierarchy = cv2.findContours(imgThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
     finalContours = []
@@ -60,8 +75,12 @@ def getContours(img, cThr=(100, 300), showCanny=False, minArea=1000, draw=False)
     if draw:
         for con in finalContours:
             cv2.drawContours(img, con[5], -1, (0, 0, 255), 3)
-            for p in con[2]:
-                cv2.drawMarker(img, (p[0, 0], p[0, 1]), (0, 200, 0), markerType=cv2.MARKER_TRIANGLE_DOWN)
+            # for i, p in enumerate(con[2]):
+            #     curP = np2tuple(con[2])
+            #     prevP = np2tuple(con[2])
+            #     cv2.drawMarker(img, curP, (0, 200, 0), markerType=cv2.MARKER_TRIANGLE_DOWN)
+            #     cv2.line(img, curP, prevP, (0, 255, 0), thickness=3)
+            cv2.polylines(img, [con[2]], True, (0, 255, 0), thickness=3)
             # # draw hull points
             # for p in con[5]:
             #     cv2.drawMarker(img, (p[0, 0], p[0, 1]), (20, 200, 200), markerType=cv2.MARKER_TILTED_CROSS)
@@ -75,10 +94,10 @@ def reorder4Corners(points):
     points = points.reshape((len(points), 2))
     add = points.sum(1)
     pointsNew[0] = points[np.argmin(add)]
-    pointsNew[3] = points[np.argmax(add)]
+    pointsNew[2] = points[np.argmax(add)]
     diff = np.diff(points, axis=1)
     pointsNew[1] = points[np.argmin(diff)]
-    pointsNew[2] = points[np.argmax(diff)]
+    pointsNew[3] = points[np.argmax(diff)]
     return pointsNew
 
 
@@ -86,7 +105,7 @@ def warpImg(img, points, w, h, prevMatrix=np.zeros((3, 3)), prevWeight=0):
     assert(len(points == 4))
     reorderedPoints = reorder4Corners(points)
     pts1 = np.float32(reorderedPoints)
-    pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+    pts2 = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     if prevMatrix.any():
         newMatrix = matrix * 1/(1+prevWeight) + prevMatrix * prevWeight/(1+prevWeight)
@@ -94,3 +113,25 @@ def warpImg(img, points, w, h, prevMatrix=np.zeros((3, 3)), prevWeight=0):
         newMatrix = matrix
     imgWarp = cv2.warpPerspective(img, newMatrix, (w, h))
     return imgWarp, newMatrix
+
+
+def transferPts(pts, M):
+    # pts should have a shape of n x 1 x 2
+    homgPts = np.array([[x, y, 1] for [[x, y]] in pts]).T
+    warpedPts = M.dot(homgPts)
+    warpedPts /= warpedPts[2]
+    warpedPts = np.array([[[round(x), round(y)]] for [x, y, _] in warpedPts.T])
+    return warpedPts
+
+
+def drawWarpedLines(pt1, pt2, M, dst, color=(0, 0, 255), thickness=2, lineType=cv2.LINE_4):
+    [wp1, wp2] = transferPts([pt1, pt2], M)
+    #print(wp1, wp2, wp1.shape, wp2.shape)
+    wp1 = np2tuple(wp1)
+    wp2 = np2tuple(wp2)
+    cv2.line(dst, wp1, wp2, color, thickness, lineType)
+
+
+def np2tuple(npPt):
+    return tuple(npPt[0, :])
+
